34845d77243e8c5c3246cb45a940965171ce6f15
Gabriel Coelho <gabrielcoelho20003@gmail.com>
2024-08-30 14:44:14 -0300

    Index, CSS e JS
    Adição de animação ao clicar em um tópico e ser direcionado para a seção no site.

--- menu.js
+++ menu.js
@@ -14,4 +14,15 @@ overlay.addEventListener('click', ()=>(
     menu.classList.remove('abrir-menu')
 ))
 
-
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        if (targetId === 'secao-inicio') {
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        } else {
+            const targetElement = document.getElementById(targetId);
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
